808c4aea5d76accab96b5f11fdcea49f15e0fa0a
Fran Hoey <fran@bluejumper.com>
2017-07-04 17:09:44 +0100

    First commit

---
+++ OfflineWebsite/OfflineWebsite/Infrastructure/CatchAllHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+
+namespace OfflineWebsite.Infrastructure
+{
+    public class CatchAllHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            SetCachingHeaders(context);
+            OutputOfflinePageHtml(context);
+        }
+
+        public bool IsReusable => true;
+
+        private void OutputOfflinePageHtml(HttpContext context)
+        {
+            context.Response.TransmitFile(ConfigurationManager.AppSettings["OfflinePageFileName"]);
+        }
+
+        private void SetCachingHeaders(HttpContext context)
+        {
+            context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            context.Response.Cache.SetNoStore();
+            context.Response.Headers.Add("Pragma", "no-cache");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Offline Website   
+
+This is a simple website that displays the `OfflinePage.html` for all requests. It is used when the main website is down for some reason and you want to show a nice message to the users
+
+## Why
+Simply putting a default.htm is the root of a website only works for requests to the root domain, any requests for sub pages will get a 404 error. It also requests the browser and proxies etc do not cache it, so when the real website returns, the users see it and not a cached version of the offline page.
+
+## Styling
+As this page will be rendered under any url, ensure references to css, js and images start "/" to root them from the website root.
